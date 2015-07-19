5da43b9da706442d279c15fad1c28a6f719da3b7
极光（polarsky） <944232369@qq.com>
2015-07-19 18:27:05 +0800

    add Content-Type and 404

--- server.js
+++ server.js
@@ -11,25 +11,49 @@ function onRequest(req, res) {
 	}
 	else {
 		var path=req.url.slice(1);
-		var t=path.charAt(0);
-		if (t=='/'||t=='.') { res.end("dont try hacking me!!"); }
+		var t=path.indexOf('..');
+		var fileType=path.slice(path.lastIndexOf('.')+1);
+		if (path.charAt(0)=='/'||t!=-1) { res.end("dont try hacking me!!"); }
 		else
 		{
 			console.log(path);
 			fs.stat(path, function(err, stats){
+				if (err)
+				{
+					res.writeHead(404, {});
+					res.end('Your querying page does not exist');
+					return;
+				}
 				console.log(stats.mtime);
 				console.log(stats.ctime);
 				console.log(Math.max((new Date(stats['mtime'])).getTime(), (new Date(stats['ctime'])).getTime()));
 				console.log(req.headers['if-modified-since']);
 				console.log(path);
 				if (!req.headers['if-modified-since']) {
-					res.writeHead(200, {
-						'Last-Modified': (new Date(Math.max((new Date(stats['mtime'])).getTime(), (new Date(stats['ctime'])).getTime()))).getTime()});
+					if (fileType=='html') {
+						res.writeHead(200, {
+							'Last-Modified': (new Date(Math.max((new Date(stats['mtime'])).getTime(), (new Date(stats['ctime'])).getTime()))).getTime(),
+							'Content-Type': 'text/html'
+						});
+					}
+					else
+					{
+						res.writeHead(200, {
+							'Last-Modified': (new Date(Math.max((new Date(stats['mtime'])).getTime(), (new Date(stats['ctime'])).getTime()))).getTime()
+						});
+					}
 					fs.createReadStream(path).pipe(res);
 				}
 				else if(Math.max((new Date(stats['mtime'])).getTime(), (new Date(stats['ctime'])).getTime()) > req.headers['if-modified-since'])
 				{
-					res.writeHead(200, {'last-modified': (new Date()).getTime()});
+					if (fileType=='html')
+					{
+						res.writeHead(200, {'last-modified': (new Date()).getTime(), 'Content-Type': 'text/html'});
+					}
+					else
+					{
+						res.writeHead(200, {'last-modified': (new Date()).getTime()});
+					}
 					fs.createReadStream(path).pipe(res);
 					console.log('200');
 				}
