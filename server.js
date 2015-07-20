0e3dfee8f460b0ec36d5b865abee2bd77ce5b328
极光（polarsky） <944232369@qq.com>
2015-07-20 15:45:15 +0800

    addwechat

--- server.js
+++ server.js
@@ -1,6 +1,106 @@
 var http = require('http');
+var https = require('https');
 var util = require('util');
 var fs   =   require('fs');
+var crypto = require('crypto');
+var access_tocken;
+var jsapi_ticket;
+var charset="abcdefghijklmnopqrstuvwxyz";
+
+var ticketRefreshing=false;
+var aTokenOptions = {
+	hostname: 'api.weixin.qq.com',
+	port: 443,
+	path: '/cgi-bin/token?grant_type=client_credential&appid=wxee703ec8c8670ca9&secret=932e32e53ceec9ffa46277734b450c33',
+	method: 'GET'
+};
+
+var ticketOptions = {
+	hostname: 'api.weixin.qq.com',
+	port: 443,
+	path: '/cgi-bin/ticket/getticket?access_token=jFlQtrYzfI-cNeguQ04fL3JMupuR6mS5sz1l61vWxgUnfIPLuC7IlQzDtgWRQOYoNeAXnzxffcsKZWOU9Geu9KhjPfd2eY0VWP6wv-0vq7I&type=jsapi',
+	method: 'GET'
+};
+
+function refreshAToken(){
+	var req = https.request(aTokenOptions, function(res) {
+
+		res.on('data', function(d) {
+			//console.log(d.toString())
+			var jr=JSON.parse(d.toString());
+			console.log(util.inspect(jr));
+			if(jr.errcode!=0)
+			{
+				access_tocken=jr.access_token;
+				ticketOptions.path='/cgi-bin/ticket/getticket?access_token='+jr.access_token+'&type=jsapi';
+				if(!ticketRefreshing)
+				{
+					ticketRefreshing=true;
+					refreshTicket();
+				}
+			}
+		});
+	});
+	req.end();
+	req.on('error', function(e) {
+		console.error(e);
+	});
+	setTimeout(refreshAToken, 3600000);
+}
+function refreshTicket(){
+	var req = https.request(ticketOptions, function(res) {
+
+		res.on('data', function(d) {
+			//console.log(d.toString())
+			var jr=JSON.parse(d.toString());
+			console.log(util.inspect(jr));
+			if(jr.errcode!=0)
+			{
+				jsapi_ticket=jr['ticket'];
+			}
+		});
+	});
+	req.end();
+	req.on('error', function(e) {
+		console.error(e);
+	});
+	setTimeout(refreshTicket, 3600000);
+}
+refreshAToken();
+
+
+String.prototype.insertData=function (data)
+{
+	var loc=this.indexOf('<!DATA_HERE>');
+	var head=this.slice(0, loc);
+	var tail=this.slice(loc+'<!DATA_HERE>'.length);
+	var dataArray=new Array();
+	for(var i in data)
+	{
+		dataArray.push(i+'="'+data[i].toString()+'";');
+	}
+	return head+dataArray.join('')+tail;
+}
+
+function generateNonce()
+{
+	var r=new Array();
+	for(var i=0;i<16;++i)
+		r.push(charset.charAt(Math.random()*26));
+	return r.join('');
+}
+function generateSignature(url, timestamp, nonceStr)
+{
+	var fourTuple=new Array();
+	fourTuple.push("jsapi_ticket="+jsapi_ticket);
+	fourTuple.push("noncestr="+nonceStr);
+	fourTuple.push("timestamp="+timestamp);
+	fourTuple.push("url="+url);
+	var content=fourTuple.join('&');
+	var sha1 = crypto.createHash('sha1');
+	sha1.update(content);
+	return sha1.digest('hex');
+}
 var server=http.createServer();
 
 function onRequest(req, res) {
@@ -14,6 +114,28 @@ function onRequest(req, res) {
 		var t=path.indexOf('..');
 		var fileType=path.slice(path.lastIndexOf('.')+1);
 		if (path.charAt(0)=='/'||t!=-1) { res.end("dont try hacking me!!"); }
+		//special entry for wechat
+		else if (path.indexOf('wechat')!=-1) {
+			var mainUrl=req.url.slice(0, req.url.indexOf('#')==-1?null:req.url.indexOf('#'));
+			var timestamp=(new Date()).getTime();
+			var nonceStr=generateNonce();
+			var signature=generateSignature(mainUrl, timestamp);
+			fs.stat(path, function(err, stats) {
+				console.log(path);
+				console.log(err);
+				var wechatBuf=new Buffer(stats.size);
+				fs.open(path, 'r', function(err, fd) {
+					if(err) console.log(err);
+					fs.read(fd, wechatBuf, 0, stats.size, 0, function(err, readBytes) {
+						if(err) throw err;
+						//else console.log(searchPageBuf.toString);
+						res.end(wechatBuf.toString().insertData({'timestamp': timestamp,'nonceStr': nonceStr,'signature': signature}));
+						fs.close(fd);
+					});
+				});
+			});
+
+		}
 		else
 		{
 			console.log(path);
@@ -68,4 +190,4 @@ function onRequest(req, res) {
 	}
 }
 server.on('request', onRequest);
-server.listen(80);
+server.listen(8011);
