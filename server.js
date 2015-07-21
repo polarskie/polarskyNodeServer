391d42e85b1bc1015f328a97c460ba864a8703a5
极光（polarsky） <944232369@qq.com>
2015-07-21 16:58:40 +0800

    xpath

--- server.js
+++ server.js
@@ -234,16 +234,3 @@ function onRequest(req, res) {
 }
 server.on('request', onRequest);
 server.listen(80);
-
-process.stdin.setEncoding('utf8');
-
-process.stdin.on('readable', function() {
-	var chunk = process.stdin.read();
-	if (chunk !== null) {
-		process.stdout.write('data: ' + chunk);
-	}
-});
-
-process.stdin.on('end', function() {
-	process.stdout.write('end');
-});
