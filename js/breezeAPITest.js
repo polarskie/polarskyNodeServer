821c6b4fe4e5c19c385fe8e33bae384b4425e73c
极光（polarsky） <944232369@qq.com>
2015-07-27 13:19:32 +0800

    o

--- js/breezeAPITest.js
+++ js/breezeAPITest.js
@@ -193,7 +193,7 @@ function showranking(){
             for(var i=0;i<rank.length;++i)
             {
                 $('#rankinglist').append('<li class="row">\
-                            <a href="#"><p style="display:inline-block; width:50%">'+rank[i]['nickname']+'</p>\
+                            <a href="#"><p style="display:inline-block; width:50%">'+rank[i]['openid']+'</p>\
                             <p style="display:inline-block; width:50%">'+rank[i]['score']+'</p></a>\
                             <a class="ui-grid-c">听</a>\
                             </li>');
