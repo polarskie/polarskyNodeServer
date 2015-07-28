654e6cefdb96f6aa59f4798215775ec827b083a1
极光（polarsky） <944232369@qq.com>
2015-07-28 16:33:16 +0800

    d

--- js/breezeAPITest.js
+++ js/breezeAPITest.js
@@ -204,7 +204,7 @@ $('#showmeonmap').click(function(){
                 longitude: res.longitude, // 经度，浮点数，范围为180 ~ -180。
                 name: 'me', // 位置名
                 address: 'i am here', // 地址详情说明
-                infoUrl: '' // 在查看位置界面底部显示的超链接,可点击跳转
+                infoUrl: 'http://www.polarsky.cc' // 在查看位置界面底部显示的超链接,可点击跳转
             });
         }
     });
