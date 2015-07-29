4952063783359ac568e0fea119d9be5a81be30fd
极光（polarsky） <944232369@qq.com>
2015-07-29 09:42:11 +0800

    b

--- js/breezeAPITest.js
+++ js/breezeAPITest.js
@@ -98,7 +98,7 @@ function uploadImg(event)
                 }
                 wx.uploadImage({
                     localId: localIds[i], // 需要上传的图片的本地ID，由chooseImage接口获得
-                    isShowProgressTips: 1, // 默认为1，显示进度提示
+                    isShowProgressTips: 0, // 默认为1，显示进度提示
                     success: function (res) {
                         var serverId = res.serverId; // 返回图片的服务器端ID
                         img.push(serverId);
