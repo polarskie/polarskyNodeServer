0826fca681504302df424c41f0ae0aef4ea66dff
极光（polarsky） <944232369@qq.com>
2015-07-29 11:10:03 +0800

    h

--- js/breezeAPITest.js
+++ js/breezeAPITest.js
@@ -64,6 +64,14 @@ $(document).on("pageinit","#challenge",function(){
                 localVoiceList.push(res.localId);
             }
         })
+        wx.checkJsApi({
+            jsApiList: ['downloadVoice'], // 需要检测的JS接口列表，所有JS接口列表见附录2,
+            success: function(res) {
+                // 以键值对的形式返回，可用的api值true，不可用为false
+                // 如：{"checkResult":{"chooseImage":true},"errMsg":"checkJsApi:ok"}
+                alert(res.checkResult.downloadVoice);
+            }
+        });
     });
     wx.config({
         'debug': 1, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
