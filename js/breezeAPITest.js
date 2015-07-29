99e71f21446881097d8385964eb063b2282f3790
极光（polarsky） <944232369@qq.com>
2015-07-29 12:17:09 +0800

    h

--- js/breezeAPITest.js
+++ js/breezeAPITest.js
@@ -220,7 +220,6 @@ $('#uploadVoice').click(function(){
 
 $('#downloadVoice').click(function ()
 {
-    /*
     function downloadVoice1(i) {
         if(i>=serverVoiceList.length)
         {
@@ -244,17 +243,6 @@ $('#downloadVoice').click(function ()
     }
     var timestart=(new Date()).getTime();
     downloadVoice1(0);
-    */
-    wx.downloadVoice({
-        serverId: '999', // 需要下载的音频的服务器端ID，由uploadVoice接口获得
-        isShowProgressTips: 1, // 默认为1，显示进度提示
-        success: function (res) {
-            var localId = res.localId; // 返回音频的本地ID
-        },
-        fail: function (){
-            alert('uu');
-        }
-    });
 });
 
 $('#getlocation').click(function()
