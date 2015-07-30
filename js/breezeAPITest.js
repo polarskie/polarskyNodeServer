9e90304f2e9316a5b9eee470c26ae42a318d7b65
极光（polarsky） <944232369@qq.com>
2015-07-30 15:23:17 +0800

    jiabutton

--- js/breezeAPITest.js
+++ js/breezeAPITest.js
@@ -353,14 +353,8 @@ function showranking(){
                             <a href="#"><p style="display:inline-block; width:50%">'+rank[i]['nickname']+'</p>\
                             <p style="display:inline-block; width:50%">'+rank[i]['score']+'</p></a>\
                             <a class="ui-grid-c" onclick="'+
-                    'listenOthers("'+voiceId+'")'
-                +'"> i</a></li>');
-                alert('<li '+icon+' class="row">\
-                            <a href="#"><p style="display:inline-block; width:50%">'+rank[i]['nickname']+'</p>\
-                            <p style="display:inline-block; width:50%">'+rank[i]['score']+'</p></a>\
-                            <a class="ui-grid-c" onclick="'+
                     'listenOthers(\''+voiceId+'\')'
-                    +'"> i</a></li>');
+                +'"> i</a></li>');
             }
             $('#jumptorankingboard').click();
             setTimeout("$('#rankinglist').listview('refresh');",0);
